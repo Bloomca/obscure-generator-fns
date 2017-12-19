ef26a73374621d24d12a4f5488cbe592f032b49c
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-19 01:09:44 +0100

    add cancellable runner

--- runners/cancel.js
+++ runners/cancel.js
@@ -1,4 +1,48 @@
-export default function cancellableRunner(genFn) {
-  const gen = genFn();
-  return new Promise((resolve, reject) => {});
+export default function runWithCancel(fn, ...args) {
+  const gen = fn(...args);
+  let cancelled, cancel;
+  const promise = new Promise((resolve, promiseReject) => {
+    // define cancel function to return it from our fn
+    cancel = () => {
+      cancelled = true;
+      reject({ reason: "cancelled" });
+    };
+
+    let value;
+
+    onFulfilled();
+
+    function onFulfilled(res) {
+      if (!cancelled) {
+        let result;
+        try {
+          result = gen.next(res);
+        } catch (e) {
+          return reject(e);
+        }
+        next(result);
+        return null;
+      }
+    }
+
+    function onRejected(err) {
+      var result;
+      try {
+        result = gen.throw(err);
+      } catch (e) {
+        return reject(e);
+      }
+      next(result);
+    }
+
+    function next({ done, value }) {
+      if (done) {
+        return resolve(value);
+      }
+      // we assume we always receive promises, so no type checks
+      return value.then(onFulfilled, onRejected);
+    }
+  });
+
+  return { promise, cancel };
 }
